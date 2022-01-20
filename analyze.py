8384d0dc7c21bb9e77b32a0092e576348c7d2018
Sriram Sami <frizensami@gmail.com>
2022-01-19 16:34:58 -0800

    Add github data retrieval and analysis scripts for blood stocks

---
+++ analyze.py
@@ -0,0 +1,85 @@
+#!/usr/bin/env python3
+import os
+from retrieve import OUTPUT_FOLDER
+import datetime
+from pprint import pprint
+import json
+from typing import List, Set, Dict, Tuple, Optional, Any
+import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
+
+
+def filename_to_datetime(filename: str):
+    file_date_str = filename.split("-")[1] + "-" + filename.split("-")[2].split(".")[0]
+    file_date = datetime.datetime.strptime(file_date_str, "%Y_%m_%d-%H_%M_%S")
+    return file_date
+
+
+# Return files in sorted order with earliest file first
+def get_sorted_filenames(folder: str) -> List[str]:
+    files = os.listdir(folder)
+    sorted_files = sorted(files, key=filename_to_datetime)
+    pprint(sorted_files)
+    print(f"Number of files: {len(sorted_files)}")
+    return sorted_files
+
+
+# For each file, process their data as json, and append to list
+def sorted_files_to_data(sorted_filenames: List[str]) -> List[Dict[str, Any]]:
+    data = []
+    for file in sorted_filenames:
+        with open(f"{OUTPUT_FOLDER}/{file}") as f:
+            json_data = json.load(f)
+            json_datetime = filename_to_datetime(file)
+            file_data = {"datetime": json_datetime, "data": json_data}
+            data.append(file_data)
+    return data
+
+
+def filedata_to_dataframe(data):
+    timestamps = list(map(lambda d: d["datetime"], data))
+    bloodtypes = list(map(lambda d: d["bloodType"], data[0]["data"]))
+    all_data = list(map(lambda d: d["data"], data))
+    df_dict = {bloodtype: [] for bloodtype in bloodtypes}
+    for data_row in all_data:
+        for bloodtype_data in data_row:
+            df_dict[bloodtype_data["bloodType"]].append(
+                int(bloodtype_data["fillLevel"][:-1])  # Remove %
+            )
+    df_dict["Time"] = timestamps
+    df = pd.DataFrame.from_dict(df_dict)
+    print(df)
+    return df
+
+
+def plot_dataframe(df):
+    # select all columns except 'rebounds' and 'assists'
+    df_positive = df.loc[:, df.columns.isin(["Time", "A+", "B+", "O+", "AB+"])]
+    df_negative = df.loc[:, df.columns.isin(["Time", "A-", "B-", "O-", "AB-"])]
+    dfm_pos = df_positive.melt("Time", var_name="Blood Type", value_name="Stock Level")
+    dfm_neg = df_negative.melt("Time", var_name="Blood Type", value_name="Stock Level")
+    sns.set_palette("bright")
+    sns.set_theme()
+    fig, axes = plt.subplots(2, 1, sharey=True)
+    axes[0].set_title("Positive Blood Groups")
+    axes[0].set_ylabel("Stock Level (%)")
+    axes[1].set_title("Negative Blood Groups")
+    axes[1].set_ylabel("Stock Level (%)")
+    fig.suptitle("Singapore Blood Stock Levels Over Time")
+    sns.lineplot(ax=axes[0], x="Time", y="Stock Level", hue="Blood Type", data=dfm_pos)
+    sns.lineplot(ax=axes[1], x="Time", y="Stock Level", hue="Blood Type", data=dfm_neg)
+    axes[0].legend(loc="lower center", title="Blood Type")
+    # sns.lineplot(data=df, x="timestamps")
+    plt.show()
+
+
+def main():
+    sorted_filenames = get_sorted_filenames(OUTPUT_FOLDER)
+    data = sorted_files_to_data(sorted_filenames)
+    df = filedata_to_dataframe(data)
+    plot_dataframe(df)
+
+
+if __name__ == "__main__":
+    main()
---
+++ retrieve.py
@@ -0,0 +1,50 @@
+#!/usr/bin/env python3
+from github import Github
+import os
+import urllib.request, json
+import time
+
+DATA_FILENAME = "blood-stocks.json"
+OUTPUT_FOLDER = "data"
+os.makedirs(OUTPUT_FOLDER, exist_ok=True)
+
+# Re
+def main(access_token: str):
+    g = Github(access_token)
+    repo = g.get_repo("datascapesg/red-cross-blood-stocks")
+    default_branch: str = repo.default_branch
+    commits = repo.get_commits()
+    for commit in commits:
+        # E.g., output: Commit(sha="4725fe2be39d4f4d280bc78d3fa233cb3f34c103")
+        print(commit)
+        # E.g., output: [File(sha="f5c654f393f6fed25270367977169cd3c537b48d", filename="blood-stocks.json")]
+        # print(commit.files)
+        commit_datetime = commit.commit.author.date.strftime("%Y_%m_%d-%H_%M_%S")
+
+        # Loop through all files added to this commit
+        for file in commit.files:
+            # Look for the flat json file
+            if file.filename == DATA_FILENAME:
+                # Get the raw URL
+                datafile_url = file.raw_url
+                # Make a network request to get the json data
+                data = []
+                with urllib.request.urlopen(datafile_url) as url:
+                    # Decode it so we can print it (sanity check)
+                    data = json.loads(url.read().decode())
+                    print(f"Writing {data} to file")
+                    with open(
+                        OUTPUT_FOLDER + f"/data-{commit_datetime}.json",
+                        "w",
+                        encoding="utf-8",
+                    ) as f:
+                        json.dump(data, f, ensure_ascii=False, indent=4)
+                # The file can't be present twice in the same commit, so we can move onto hte next commit
+                break
+
+    pass
+
+
+if __name__ == "__main__":
+    access_token = os.environ["TOKEN"]
+    main(access_token)
